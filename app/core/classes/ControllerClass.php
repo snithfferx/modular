37df5dfeb2fdae6206a66f3bc90d03e87c835d5e
JorgeDS <jrivera@piensads.com>
2022-12-28 17:32:29 -0600

    Mejora | Class: Controller | Agregado componente en comun, Funcionalidad de view, Respuesta de controlador

--- app/core/classes/ControllerClass.php
+++ app/core/classes/ControllerClass.php
@@ -1,32 +1,35 @@
 <?php
     namespace app\core\classes;
-    use app\core\helper\configHelper;
+    use app\core\helpers\ConfigHelper;
+    use app\core\helpers\MessengerHelper;
     use app\core\libraries\AuthenticationLibrary;
+    require "../helpers/DefinerHelper.php";
     /**
      * Clase que analiza y busca el controlador en la aplicación.
      * @category File
      * @author Jorge Rivera
-     * @version 1.1.0
+     * @package app\core\classes\ControllerClass
+     * @version 1.3.0 dev r1
      */
     class ControllerClass {
         /**
          * Contiene el nombre del controlador al cuál se le hace la petición
-         * @var string controller
+         * @var string $controller
          */
         private $controller;
         /**
          * Contiene el nombre del método al cual se le realiza la petición
-         * @var string method
+         * @var string $method
          */
         private $method;
         /**
          * Contiene la petición a realizarse
-         * @var string|array params
+         * @var string|array $params
          */
         private $params;
         /**
          * Contiene el arreglo de la configuración de la aplicación
-         * @var array $configs
+         * @var object $configs
          */
         private $configs;
         /**
@@ -44,6 +47,15 @@
          * @var string $class
          */
         private $class;
+        /**
+         * Helper Messenger
+         * @var object $messenger
+         */
+        private $messenger;
+        public function __construct() {
+            $this->messenger = new MessengerHelper;
+            $this->configs = new ConfigHelper;
+        }
         /**
          * Función parar resolver la peticón al controlador
          * @param array $values
@@ -71,89 +83,103 @@
                     $this->controller,
                     $this->class,
                     $this->method,
-                    $this->params);/* 
-                $response = "Petici&oacute;n realizada con m&eacute;todo: $this->serverMethod.<br>
-                    Hacia el m&oacute;dulo: $this->module.<br>
-                    Usando el controller: $this->controller.<br>
-                    Con Clase: $this->class.<br>
-                    Usando la funci&oacute;n: $this->method.<br>
-                    Con parametros: $this->params."; */
+                    $this->params);
             } else {
-                $response = $this->getDefaultResponse();
+                $response = $this->messenger->build(['type'=>"error",'data'=>['code'=>404,'message'=>"No request."]]);
             }
             return $response;
         }
         public function getDefaultResponse () {
-            return "Respuesta predeterminada";
+            return $this->getControllerResponse("default");
         }
-        public function view () {
-            
-        }
-        public function getController () {}
-        //public function get
-
-        protected function getControllerResponse (string $module, string $controller, string $class, string $method, $params) {
-            if (!empty($module)) {
-                if ($module == "default") {
-                    $module = $this->getConfigVars['default_ctr'];
-                }
-                if (empty($controller) || is_null($controller)) {
-                    $response = $this->createMessage("view",[
-                        'type'=>"error",
-                        'message'=>"Error, el modulo no existe"]);
-                } else {
-                    $path = _MODULE_;
-                    $controller = ucfirst($controller) . "Controller";
-                    $path .= ucfirst($module) . "Module/";
-                    $path .= "$controller.php";
-                    if (file_exists($path)) {
-                        try {
-                            require_once($path);
-                            if (class_exists($class)) {
-                                $ctr = new $class;
-                                if (method_exists($ctr,$method)) {
-                                    try {
-                                        $response = $ctr->$method($params);
-                                    } catch (\Exception $exepcion) {
-                                        $response = $this->createMessage("view",[
-                                            'type'=>"error",
-                                            'message'=>$exepcion->getMessage()]);
-                                    }
-                                } else {
-
-                                }
-                            } else {
-                                $response = $this->createMessage("view",[
-                                    'type'=>"error",
-                                    'message'=>"Lo sentimos, el metodo no existe"]);
-                            }
-                        } catch (\Exception $exepcion) {
-                            $response = $this->createMessage("view",[
-                                'type'=>"error",
-                                'message'=>$exepcion->getMessage()]);
-                        }
-                    } else {
-                        $response = $this->createMessage("view",[
-                            'type'=>"error",
-                            'message'=>"Error, el modulo no existe"]);
-                    }
-                }
+        public function getControllerInstance(string $controller)
+        {
+            $name = explode("/", $controller);
+            if (count($name) > 2) {
+                $response = $this->getComponent($name[0], $name[1], $name[2]);
+            } elseif (count($name) > 1) {
+                $response = $this->getComponent($name[0], $name[1]);
             } else {
-                $response = $this->createMessage("view",[
-                    'type'=>"error",
-                    'message'=>"Error, el modulo no existe"]);
+                $response = $this->getComponent($controller);
             }
             return $response;
         }
-        protected function createMessage () {
-            return "holea";
+        public function getModelInstance(string $model)
+        {
+            $name = explode("/", $model);
+            if (count($name) > 2) {
+                $response = $this->getComponent($name[0],"models" , $name[1], $name[2]);
+            } elseif (count($name) > 1) {
+                $response = $this->getComponent($name[0], "models", $name[1]);
+            } else {
+                $response = $this->getComponent( $model, "models");
+            }
+            return $response;
         }
-        protected function isMethod () {
+        //public function get
 
+        protected function getControllerResponse (string $module, string $controller = null, string $class=null, string $method = "index", $params = null) :array {
+            if (!empty($module)) {
+                if ($module == "default") {
+                    $vars = $this->configs->get();
+                    $module = $vars['default_ctr'];
+                    $controller = $vars['default_ctr'];
+                    $class = $vars['default_ctr'];
+                }
+                if (empty($controller) || is_null($controller)) {
+                    $response = $this->messenger->build([
+                        'type'=>"error",
+                        'data'=>[
+                            'code'=>404,
+                            'message'=>"Error, el modulo no existe"]]);
+                } else {
+                    $controllerInstance = $this->getComponent($module,$controller,$class);
+                    if (!is_array($controllerInstance)) {
+                        if (method_exists($controllerInstance,$method)) {
+                            try {
+                                $response = $controllerInstance->$method($params);
+                            } catch (\Exception $exepcion) {
+                                $response = $this->messenger->build(['type'=>"erro",'data'=>[
+                                    'code'=>404,
+                                    'message'=>$exepcion->getMessage()]]);
+                            }
+                        } else {
+                            $response = $this->messenger->build(['type'=>"erro",'data'=>['code'=>404,
+                                'message'=>"Method Requested Is Not Valid"]]);
+                        }
+                    }
+                    return $controllerInstance;
+                }
+            } else {
+                $response = $this->messenger->build(['type'=>"error",'data'=>['code'=>404,
+                    'message'=>"Error, el modulo no existe"]]);
+            }
+            return $response;
         }
-        private function getConfigVars () {
-            //$cnfg = new configHelper;
-            //$this->configs = $cnfg->getConfigVars();
+        protected function getComponent (string $moduleName, string $type = "controllers", string $componentName = null, string $className = null) {
+            $path = _MODULE_;
+            $componentFile = ucfirst($componentName);
+            if (!is_null($className) && $className != $componentFile) $componentFile .= "_" . $className . "Controller";
+            $path .= ucfirst($moduleName) . "/$type/";
+            $path .= "$componentFile.php";
+            if (file_exists($path)) {
+                try {
+                    $componentNameSpace = "app/core/modules/$moduleName/$type/$componentFile";
+                    $componentToUse = str_replace('/', '\\', $componentNameSpace);
+                    $response = new $componentToUse;
+                } catch (\Exception $exepcion) {
+                    $response = $this->messenger->build(['type'=>"error", 'data'=>[
+                        'code' => 404,
+                        'message' => $exepcion->getMessage()]
+                    ]);
+                }
+            } else {
+                $response = $this->messenger->build(['type' => "error", 'data' => [
+                    'code' => 404,
+                    'message' => "Error, el modulo no existe"
+                ]]);
+            }
+            return $response;
         }
     }
 ?>
