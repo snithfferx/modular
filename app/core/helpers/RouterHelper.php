23c0abce258d8f9ec57a8688426b9f58eff34b3f
JorgeDS <jrivera@piensads.com>
2022-12-28 17:36:53 -0600

    Feature | Helper: Router | Agregado solucionador de rutas

---
+++ app/core/helpers/RouterHelper.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * @category Helper
+ * @author JEcheverria <jecheverria@piensads.com>
+ * @version 1.0.0
+ * Clase que resuelve la ruta proporcionada.
+ */
+namespace app\core\helpers;
+/**
+ * Summary of RouterHelper
+ * @package app\core\helpers\RouterHelper
+ */
+class RouterHelper {
+    private $qmPos;
+    private $url;
+    public function getPath() {
+        $this->url = $_SERVER['REQUEST_URI'] ?? '/';
+        $this->qmPos = strpos($this->url, '?');
+        return ($this->qmPos === false) ? $this->url : substr($this->url, 1, ($this->qmPos - 1));
+    }
+    public function getMethod()
+    {
+        return
+        strtolower($_SERVER['REQUEST_METHOD']);
+    }
+    public function resolve () {
+        $path = $this->getPath();
+        $method = $this->getMethod();
+        if ($path == "/") {
+            $response = [
+                'sv_method' => $method,
+                'app_module' => "home",
+                'app_class' => "home",
+                'app_method' => "index",
+                'app_params' => null
+            ];
+        } else {
+            $pathArray = explode('/', $path);
+            if ($this->qmPos != false) {
+                $prms = substr($this->url, $this->qmPos);
+            } else {
+                //array_shift($pathArray);
+                $prms = null;
+            }
+            $mtd = "index";
+            $pathArraySize = sizeof($pathArray);
+            $mdl = $pathArray[0];
+            $ctr = $pathArray[0];
+            $clss = $pathArray[0];
+            $response = [];
+            switch ($pathArraySize) {
+                case 5:
+                    $ctr = $pathArray[1];
+                    $clss = $pathArray[2];
+                    $mtd = $pathArray[3];
+                    $prms = $pathArray[4];
+                    break;
+                case 4:
+                    $clss = $pathArray[1];
+                    $mtd = $pathArray[2];
+                    $prms = $pathArray[3];
+                    break;
+                case 3:
+                    $mtd = $pathArray[1];
+                    $prms = $pathArray[2];
+                    break;
+                default:
+                    $mtd = $pathArray[1];
+                    break;
+            }
+            $response = [
+                'sv_method' => $method,
+                'app_module' => $mdl,
+                'app_controller' => $ctr,
+                'app_class' => $clss,
+                'app_method' => $mtd,
+                'app_params' => $prms
+            ];
+        }
+        return $response;
+    }
+}
