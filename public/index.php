720a0e027994e3f877ae9fa5376918afd22f05e0
snithfferx <snithfferx@outlook.com>
2023-11-06 01:47:01 -0600

    Actualizacion de Index

--- public/index.php
+++ public/index.php
@@ -1,18 +1,14 @@
 <?php
-    /**
-     * CIRACO
-     * @author Jorge Echeverria <snithfferx@outlook.com>
-     * @version 1.0.0
-     */
-    require __DIR__ . '/../../vendor/autoload.php';
-    use app\core\LoaderClass;
-    $app = new LoaderClass;
-    $app->init();
-    $response = $app->verifyRequest();
-    echo $app->display($response);
-    /* echo "<pre>";
-    var_dump($response);
-    echo "</pre>";
-    exit; */
-    $app->terminate();
-?>
+/**
+ * @category Application
+ * @package  Application
+ * @author   snirthfferx <jecheverria@bytes4run.com>
+ * @version  1.0.0
+ */
+require '../vendor/autoload.php';
+use app\Loader;
+$app = new Loader();
+$app->init();
+$response = $app->run();
+$app->render($response);
+$app->end();
